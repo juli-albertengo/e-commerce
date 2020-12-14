8535de444e5b305eb55f2542e97def921916d3e3
Julieta Albertengo <albertengo.juli@gmail.com>
2020-12-14 18:56:34 -0300

    Entrega clase 13, proceso de compra con datos estaticos

--- src/containers/CartContainer/CartContainer.js
+++ src/containers/CartContainer/CartContainer.js
@@ -9,11 +9,35 @@ import { NavLink } from 'react-router-dom'
 
 //Context
 import {useAppContext} from '../../context/useAppContext'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+
+//Set orders
+import { getFirestore } from '../../firebase';
+import firebase from 'firebase/app';
 
 function CartContainer(){
     const {cart, getCartTotal} = useAppContext();
     const [total, setTotal] = useState(getCartTotal(cart))
+
+    const processPurchase = () => {
+        console.log(cart);
+        const db = getFirestore();
+        const ventas = db.collection("orders");
+        const newOrder = {
+            buyer: {name: 'Julieta Albertengo', email: 'julieta@prueba.com'},
+            products: {cart},
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            total: getCartTotal(cart) 
+        }
+        ventas.add(newOrder).then(({id}) => {
+            console.log(id);
+        }).catch(err => {
+            console.log(err)
+        }).finally(() => {
+            console.log('Cambiar Loading a False');
+        })
+    }
+
 
     return(
         <div className='container-fluid'>
@@ -30,7 +54,7 @@ function CartContainer(){
                 }) 
             }
             <h4 className='bajada'>Total Amount: ${total}</h4>
-            <button className='mb-3'> Process Purchase</button>
+            <button className='mb-3' onClick={processPurchase}> Process Purchase</button>
             {
             //Detalle del producto, foto con nombre, precio a la derecha,
             //Poder agregar mas items del mismo libro
